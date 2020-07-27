544e1875386c2de3bc872117e6c289b33d3aebda
Fayazazeemp <fayazazeem444555@gmail.com>
2020-07-27 15:09:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/AdapterNews.java
@@ -0,0 +1,72 @@
+package com.example.newsapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class AdapterNews extends RecyclerView.Adapter<AdapterNews.NewsVH> {
+
+    Context context;
+    ArrayList<ModelNews> data;
+
+    public AdapterNews(Context context, ArrayList<ModelNews> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public NewsVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_news_item, parent, false);
+        NewsVH newsVH = new NewsVH(view);
+        return newsVH;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsVH holder, int position) {
+        final ModelNews dataItem = data.get(position);
+        holder.txtTitle.setText(dataItem.getNewsTitle());
+        holder.txtDesc.setText(dataItem.getNewsDesc());
+        Glide.with(context).load(dataItem.getNewsImg()).into(holder.imgNews);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newIntent = new Intent(context,NewsDetailsActivity.class);
+                newIntent.putExtra("URL",dataItem.getNewsURL());
+                context.startActivity(newIntent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class NewsVH extends RecyclerView.ViewHolder {
+
+        TextView txtTitle, txtDesc;
+        ImageView imgNews;
+
+        public NewsVH(@NonNull View itemView) {
+            super(itemView);
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            txtDesc = itemView.findViewById(R.id.txtDesc);
+            imgNews = itemView.findViewById(R.id.newsImg);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    String API_KEY = "b066a94f47384f3e818eb99b5c2ef032"; // Sign up to https://newsapi.org/ to get API KEY and paste it here
+    String API_URL = "https://newsapi.org/v2/top-headlines?country=in&apiKey=" + API_KEY;
+
+    RecyclerView newsRecycler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        newsRecycler = findViewById(R.id.recyclerNews);
+
+
+        final ProgressDialog loader = new ProgressDialog(this);
+        loader.setMessage("Loading data...");
+        loader.setCancelable(false);
+        loader.show();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, API_URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                loader.dismiss();
+                handleResponse(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                loader.dismiss();
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                Log.e("ERROR", error.toString());
+            }
+        });
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+
+    }
+
+    private void handleResponse(JSONObject response) {
+
+        ArrayList<ModelNews> newsArray = new ArrayList<>();
+
+        try {
+            JSONArray articlesArray = response.getJSONArray("articles");
+            for (int i = 0; i < articlesArray.length(); i++) {
+                JSONObject newsObject = (JSONObject) articlesArray.get(i);
+                String newsTitle = newsObject.getString("title");
+                String newsDesc = newsObject.getString("description");
+                String newsURL = newsObject.getString("url");
+                String newsImageURL = newsObject.getString("urlToImage");
+
+                ModelNews newsItem = new ModelNews(newsTitle, newsDesc, newsImageURL, newsURL);
+                newsArray.add(newsItem);
+            }
+
+            newsRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext(), RecyclerView.VERTICAL, false));
+            newsRecycler.setAdapter(new AdapterNews(getApplicationContext(), newsArray));
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Inside Exception block", Toast.LENGTH_SHORT).show();
+        }
+
+
+    /*
+    try{
+      //will do some task, if any error occured jumbps to catch block
+    }catch (Exception e){
+        //In case of any error, this block will be excuted
+    }*/
+
+    }
+}
